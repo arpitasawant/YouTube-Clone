552b5bcaec928ef6a8cec371e58215d15e31f198
Arpita Sawant <97539573+arpitasawant@users.noreply.github.com>
2023-12-02 05:36:34 +0530

    Update contextApi.js

--- src/context/contextApi.js
+++ src/context/contextApi.js
@@ -2,7 +2,9 @@ import React, { createContext, useState, useEffect } from "react";
 
 import { fetchDataFromApi } from "../utils/api";
 export const Context = createContext();
-//fetching the data
+
+//Fetching the data
+
 export const AppContext = (props) => {
     const [loading, setLoading] = useState(false);
     const [searchResults, setSearchResults] = useState([]);
